28d7e769c0c25a48396618b82a2a86ae90068a2b
Jadoiconic <86121130+MustaDevPro@users.noreply.github.com>
2023-01-05 23:54:24 +0200

    upcoming and trending

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,11 @@
 import axios from 'axios'
+import {FaHeart, FaRegHeart } from 'react-icons/fa'
 import React, { useEffect, useState } from 'react'
 
 const Row = ({ title, fetchUrl }) => {
     const [movies, setMovies] = useState([])
+    const [like, setLike] = useState(false)
+    
 
     useEffect(() => {
         axios.get(fetchUrl).then(res => {
@@ -19,7 +22,13 @@ const Row = ({ title, fetchUrl }) => {
                 <div id={'slider'}>
                     {movies.map((item, id) => (
                         <div className='w-[160px] sm:2-[200px] md:w-[240px] lg:[280px] inline-block cursor-pointer relative p-2'>
-                            <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
+                            <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item.title} />
+                            <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100'>
+                                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item.title} </p>
+                                <p>
+                                    {like ? <FaHeart className='absolute top-4 left-4 text-gray-300' /> : <FaRegHeart className='absolute top-4 left-4 text-gray-300' />}
+                                </p>
+                            </div>
                         </div>
                     ))}
                 </div>
